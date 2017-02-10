9a2f8cb3aaf16886e227d99a233bc595067f06bb
xin <xinliqun@weizoom.com>
2017-02-10 16:20:35 +0800

    django 实践第一发

---
+++ helloworld/helloworld/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import *
+from helloworld.view import hello
+from helloworld.testdb import testdb
+from helloworld.search import *
+from helloworld import search2
+
+urlpatterns = patterns("",
+		('^hello/$',hello),
+		('^testdb/$', testdb),
+		(r'^search_form/$', search_form),
+		(r'^search/$', search),
+		(r'^search-post/$', search2.search_post),
+	)
