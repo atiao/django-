5af0a4b19573c0954cc6f3b5ed0fa4c431e4e56b
xin <xinliqun@weizoom.com>
2017-02-13 17:27:36 +0800

    登陆竟然成功了？！

--- helloworld/TestModel/models.py
+++ helloworld/TestModel/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Test(models.Model):
-	name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
--- helloworld/helloworld/urls.py
+++ helloworld/helloworld/urls.py
@@ -4,10 +4,15 @@ from helloworld.testdb import testdb
 from helloworld.search import *
 from helloworld import search2
 
+from django.contrib import admin
+admin.autodiscover()
+
 urlpatterns = patterns("",
 		('^hello/$',hello),
 		('^testdb/$', testdb),
 		(r'^search_form/$', search_form),
 		(r'^search/$', search),
 		(r'^search-post/$', search2.search_post),
+		url(r'^admin/', include(admin.site.urls)),
+		url(r'^Users/', include('Users.urls')),
 	)
